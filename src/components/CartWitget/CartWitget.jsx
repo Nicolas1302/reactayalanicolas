98fe469b5ae18a28e77dab91e7033ff6e6f0313e
Nicolas1302 <nicoebon@gmail.com>
2023-05-23 23:14:38 -0300

    NavBar, Cartwidget, itemListContainer

---
+++ src/components/CartWitget/CartWitget.jsx
@@ -0,0 +1,12 @@
+import cart from './assets/cartwidget.jpg'
+import './cartwidget.css'
+
+const CardWitget = () => {
+    return (
+        <div>
+            <img className='imagen' src = {cart} alt="logo cartwidget" />
+        </div>
+    )
+}
+
+export default CardWitget
